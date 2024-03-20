c93b5f9559dc3ad10154309e27c02c16b4acf799
Noemie Jankowski <noemie@MacBook-Air-de-Noemie.local>
2024-03-20 16:05:57 +0100

    erreur

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -15,21 +15,19 @@ export default class NewBill {
     this.billId = null
     new Logout({ document, localStorage, onNavigate })
   }
+
   handleChangeFile = e => {
     e.preventDefault()
-    const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
+    const inputFile = this.document.querySelector(`input[data-testid="file"]`)
+    const file = inputFile.files[0]
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
+    
+    if (fileName.match(/(\.jpg|\.jpeg|\.png)$/)) {
     const formData = new FormData()
     const email = JSON.parse(localStorage.getItem("user")).email
     formData.append('file', file)
     formData.append('email', email)
-
-    const fileExtension = file.name.split(".").pop().toLowerCase();
-    if (["jpg", "jpeg", "png"].indexOf(fileExtension) === -1) {
-      alert("Seuls les fichiers .jpg, .jpeg et .png sont autorisés.");
-      return;
-    }
 
     this.store
       .bills()
@@ -45,6 +43,10 @@ export default class NewBill {
         this.fileUrl = fileUrl
         this.fileName = fileName
       }).catch(error => console.error(error))
+    } else {
+      alert("Veuillez entrer un fichier JPG, JPEG, ou PNG")
+      inputFile.value = ""
+    }
   }
   handleSubmit = e => {
     e.preventDefault()
