e48cffba964116c9eaef2080c9ce8fc816d6b9e0
Noemie Jankowski <noemie@MacBook-Air-de-Noemie.local>
2024-03-20 16:28:35 +0100

    bug 3

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -18,9 +18,13 @@ export default class NewBill {
 
   handleChangeFile = e => {
     e.preventDefault()
-    const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
+    const inputFile = this.document.querySelector(`input[data-testid="file"]`)
+    const file = inputFile.files[0]
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
+
+    if (fileName.match(/(\.jpg|\.jpeg|\.png)$/)) {
+
     const formData = new FormData()
     const email = JSON.parse(localStorage.getItem("user")).email
     formData.append('file', file)
@@ -40,7 +44,10 @@ export default class NewBill {
         this.fileUrl = fileUrl
         this.fileName = fileName
       }).catch(error => console.error(error))
-   
+    } else {
+      alert("Veuillez entrer un fichier JPG, JPEG, ou PNG")
+      inputFile.value = ""
+    }
   }
   handleSubmit = e => {
     e.preventDefault()
